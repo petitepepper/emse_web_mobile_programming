203a9c81446b92b9b973ffd75fdc0d2ba7befa75
petitepepper <meijiajing623@163.com>
2021-11-14 15:34:56 +0100

    [Update] add @CrossOrigin to controllers

--- faircorp/src/main/java/com/emse/spring/faircorp/api/BuildingController.java
+++ faircorp/src/main/java/com/emse/spring/faircorp/api/BuildingController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/buildings")
 @Transactional
